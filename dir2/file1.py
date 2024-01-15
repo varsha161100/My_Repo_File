3eefa48d04bba3030f4d398e42619003e4e5e293
varsha161100 <154409013+varsha161100@users.noreply.github.com>
2024-01-15 14:48:08 +0530

    First commit

---
+++ dir2/file1.py
@@ -0,0 +1,10 @@
+def myfan(a):
+    print("value =",a)
+    a=a+2
+    print("value after adding 2= ",a)
+    return a
+
+a=5
+print("value n=",a)
+myfan(a) 
+print("value n=",a)
---
+++ dir2/file2.py
@@ -0,0 +1,7 @@
+a=[2,5,8,4,9]
+for i in a:
+ print(i)
+
+obj=[44,67,89,"hello","varsha","Rawat"]
+for i in obj:
+ print(obj[3])
